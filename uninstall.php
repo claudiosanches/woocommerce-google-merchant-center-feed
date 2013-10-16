5369bb658edf077fa4f312e38ac8d004f0a3bbed
claudiosmweb <contato@claudiosmweb.com>
2013-10-16 20:47:51 -0300

    added uninstall file

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+// If uninstall not called from WordPress exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) || ! WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
+
+    status_header( 404 );
+    exit;
+}
+
+// Delete feed page.
+$feed_slug = sanitize_title( __( 'product-feed', 'wcugpf' ) );
+$feed_page = get_page_by_path( $feed_slug );
+if ( $feed_page )
+    wp_delete_post( $feed_page->ID, true );
